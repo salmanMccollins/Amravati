13e1a780a1a877bfc41b22fb0ff2caa8f7c2b53d
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-01-08 15:31:21 +0530

    ffff

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,10 +1,14 @@
 import PageHeader from "@/components/common/PageHeader";
+import InstaBlock from "@/components/home-components/InstaBlock";
+import Testimonials from "@/components/home-components/Testimonials";
 export default function aboutus() {
   return (
     <>
       
       <div>
         <PageHeader title="about us"/>
+        <Testimonials/>
+        <InstaBlock/>
         </div>
     </>
   );
